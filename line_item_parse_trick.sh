3e376a0cb0e42d4f6efc39130fb68abcc5f15113
Victor Yan <vicyan614@gmail.com>
2022-02-17 00:32:46 -0500

    Create line_item_parse_trick.sh

---
+++ line_item_parse_trick.sh
@@ -0,0 +1,7 @@
+# Pipe previous stdout line into a line reader, breakdown line by delimiter " ", utilize individual value freely
+ls -ld * | 
+ while IFS= read -r line; do 
+   ninth=$(echo $line | cut -d " " -f9)
+   eighth=$(echo $line | cut -d " " -f8)
+   echo now I can use $eighth and $ninth item however I want 
+ done
