bd0587915307dfca85a2c39c7cd1a4b5645233f3
Victor Yan <vicyan614@gmail.com>
2022-02-17 00:33:16 -0500

    Update line_item_parse_trick.sh

--- line_item_parse_trick.sh
+++ line_item_parse_trick.sh
@@ -1,4 +1,4 @@
-# Pipe previous stdout line into a line reader, breakdown line by delimiter " ", utilize individual value freely
+# Pipe previous stdout lines into a line reader, breakdown each line by delimiter " ", utilize individual value freely
 ls -ld * | 
  while IFS= read -r line; do 
    ninth=$(echo $line | cut -d " " -f9)
